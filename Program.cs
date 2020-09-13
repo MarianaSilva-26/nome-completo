d5dfc407c15576444cc39ff683783cf82a42eb9d
marianaSilva-26 <mariana.silvadossantos.26@gmail.com>
2020-09-13 19:37:18 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace nomecompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite seu primeiro nome:");
+            string nome=Console.ReadLine();
+            Console.Write("digite seu sobrenome:");
+            string sobrenome=Console.ReadLine();
+            Console.WriteLine("nome completo:"+nome+" "+sobrenome);
+            Console.Write("nome do catálogo:"+sobrenome+", "+nome);
+            
+        }
+    }
+}
